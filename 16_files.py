04df7f1deaab23c16a2fa03f8e05007e4aac655c
tomer2b <60958579+tomer2b@users.noreply.github.com>
2024-04-22 16:01:39 +0300

    Update 16_files.py

--- 16_files.py
+++ 16_files.py
@@ -1,3 +1,4 @@
+# פעולה הקוראת תוכן של קובץ שורה אחרי שורה
 def reading_method1(file_name):
   file = open(file_name,"r")
   for line in file:
@@ -5,20 +6,21 @@ def reading_method1(file_name):
   file.close
 
 
-# all file into one string
+# פעולה הקוראת תוכן של קובץ 
 def reading_method2(file_name):
   file = open(file_name,"r")
-  content = file.readline()
+  content = file.read()
   print (content)
   file.close
 
+# פעולה הקוראת שורה מתוך קובץ 
 def reading_method3(file_name):
   with open(file_name,"r") as file:
     content = file.readline()
     print (content)
 
 
-# write to end of file 
+# כתיבה לסוף של קובץ 
 def write_to_file(file_name):
   f = open(file_name, "a")
   f.write("write some content!")
