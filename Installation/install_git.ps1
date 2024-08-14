1b42c93abe7ea85db30516bf5c57903d2e4b28ee
tomer2b <60958579+tomer2b@users.noreply.github.com>
2024-08-14 09:46:34 +0300

    Delete Installation directory

--- Installation/install_git.ps1
+++
@@ -1,8 +0,0 @@
-$url = "https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe"
-$git_dest = "Git-2.43.0-64-bit.exe"
-Invoke-RestMethod -Uri $url -OutFile $git_dest
-
-
-
-Start-Process -FilePath $git_dest -Argument "/VERYSILENT /NORESTART"
-
--- Installation/updateSettingsJSON.ps1
+++
@@ -1,6 +0,0 @@
-$url = "https://raw.githubusercontent.com/RabinSchool/python_examples/main/settings.json"
-$dest = "$Env:USERPROFILE\appdata\Roaming\code\user\settings.json"
-Invoke-RestMethod -Uri $url -OutFile $dest
-pause
-
-
