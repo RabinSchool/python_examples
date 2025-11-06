aa9827ce71ea1e71b8f8ebe5255a2196d1416a7e
tomer2b <60958579+tomer2b@users.noreply.github.com>
2025-11-06 14:45:11 +0200

    Update 08_Functions.py

--- 08_Functions.py
+++ 08_Functions.py
@@ -6,7 +6,6 @@ def ex1_print_hello():
 
 ex1_print_hello()
 
-
 #----------------------------------------------
 # הפעולה המקבלת פרמטר ואינה מחזיר ערך
 
@@ -14,7 +13,6 @@ def print_full_name(first_name,last_name):
     print (first_name,last_name)
 
 print_full_name('Tomer','Tubi')
-
 
 #----------------------------------------------
 # פעולה המקבלת פרמטר ומחזירה ערך
@@ -24,4 +22,5 @@ def sum_numbers(x,y):
 # שימו לב כי חייב להופיע משתנה שישמור את ערך החזרה של הפעולה
 
 total = sum_numbers(32,45)
+
 
